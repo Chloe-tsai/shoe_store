4c6f96c62b42bba0d5b50098a3ba556b6ce016d0
Chloe-tsai <hsingjung0306@gmail.com>
2026-08-19 14:54:20 +0800

    更新AOS

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 import './assets/scss/all.scss';
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 console.log('Hello world');
+
+AOS.init({
+  offset: 120,
+  duration: 400,
+  once: false,
+  delay: 100,
+});
